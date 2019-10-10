582440bf1e7e0a779e40b1d10c35047908c8e45f
nami sabet <nami.sabet@hotmail.com>
2019-10-10 15:09:37 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseForFred;
+
+namespace DatabaseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myProduct = DatabaseForFred.DB.GetProduct(1);
+        }
+    }
+}
